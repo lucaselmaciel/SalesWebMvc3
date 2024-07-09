eae659a56f7dee01d9414a62d1698cfea327a152
Lucas Maciel <lucasmaciel63@gmail.com>
2024-07-09 11:14:33 -0300

    Add project files.

---
+++ SalesWebMvc3/Data/SalesWebMvc3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc3.Models;
+
+namespace SalesWebMvc3.Data
+{
+    public class SalesWebMvc3Context : DbContext
+    {
+        public SalesWebMvc3Context (DbContextOptions<SalesWebMvc3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvc3.Models.Department> Department { get; set; } = default!;
+    }
+}
