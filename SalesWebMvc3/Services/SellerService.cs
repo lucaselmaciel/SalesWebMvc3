accfc04f3169ddef03f4de0e58c881d68ea36562
Lucas Maciel <lucasmaciel63@gmail.com>
2024-07-10 10:25:36 -0300

    sellers service

---
+++ SalesWebMvc3/Services/SellerService.cs
@@ -0,0 +1,20 @@
+﻿using SalesWebMvc3.Data;
+using SalesWebMvc3.Models;
+
+namespace SalesWebMvc3.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvc3Context _context;
+
+        public SellerService(SalesWebMvc3Context context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
