281b89bd9972bf5f7a4371f25d1983d6b27d8444
Lucas Maciel <lucasmaciel63@gmail.com>
2024-07-10 15:50:30 -0300

    seller details

--- SalesWebMvc3/Services/SellerService.cs
+++ SalesWebMvc3/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMvc3.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc3.Data;
 using SalesWebMvc3.Models;
 
 namespace SalesWebMvc3.Services
@@ -25,7 +26,7 @@ namespace SalesWebMvc3.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
