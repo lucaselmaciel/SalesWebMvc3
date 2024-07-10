9cf44f90dd30fa81a38d86a4e4aecb6e4b2f8ec7
Lucas Maciel <lucasmaciel63@gmail.com>
2024-07-10 08:39:14 -0300

    migrations

--- SalesWebMvc3/Migrations/20240710113630_OtherEntities.Designer.cs
+++ SalesWebMvc3/Migrations/20240710113630_OtherEntities.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SalesWebMvc3.Data;
@@ -11,9 +12,11 @@ using SalesWebMvc3.Data;
 namespace SalesWebMvc3.Migrations
 {
     [DbContext(typeof(SalesWebMvc3Context))]
-    partial class SalesWebMvc3ContextModelSnapshot : ModelSnapshot
+    [Migration("20240710113630_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
