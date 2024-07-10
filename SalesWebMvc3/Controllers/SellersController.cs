a89668242f52560a0a7dfde779588c25412c3c32
Lucas Maciel <lucasmaciel63@gmail.com>
2024-07-10 10:25:26 -0300

    Sellers controller

--- SalesWebMvc3/Controllers/SellersController.cs
+++ SalesWebMvc3/Controllers/SellersController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc3.Models;
+using SalesWebMvc3.Services;
 
 namespace SalesWebMvc3.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService _sellerService;
+        
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Seller> sellersList = _sellerService.FindAll();
+            return View(sellersList);
         }
     }
 }
--- SalesWebMvc3/Program.cs
+++ SalesWebMvc3/Program.cs
@@ -1,32 +1,41 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc3.Data;
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<SalesWebMvc3Context>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("SalesWebMvc3Context") ?? throw new InvalidOperationException("Connection string 'SalesWebMvc3Context' not found.")));
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+using SalesWebMvc3.Services;
+internal class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddDbContext<SalesWebMvc3Context>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("SalesWebMvc3Context") ?? throw new InvalidOperationException("Connection string 'SalesWebMvc3Context' not found.")));
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddScoped<SellerService>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
