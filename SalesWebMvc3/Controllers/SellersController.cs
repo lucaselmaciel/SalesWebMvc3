d7b56e568f8da5454480168c31bd41291088a462
Lucas Maciel <lucasmaciel63@gmail.com>
2024-07-11 08:50:01 -0300

    seller edition to controller

--- SalesWebMvc3/Controllers/SellersController.cs
+++ SalesWebMvc3/Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using SalesWebMvc3.Models;
 using SalesWebMvc3.Models.ViewModels;
 using SalesWebMvc3.Services;
+using SalesWebMvc3.Services.Exceptions;
 
 namespace SalesWebMvc3.Controllers
 {
@@ -83,5 +84,51 @@ namespace SalesWebMvc3.Controllers
 
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Seller obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller =  obj, Departments = departments };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            
+            try
+            {
+                DateTime dateTime = seller.BirthDate;
+                if (dateTime.Kind == DateTimeKind.Unspecified)
+                {
+                    dateTime = DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+                }
+                dateTime = dateTime.ToUniversalTime();
+                seller.BirthDate = dateTime;
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
     }
 }
